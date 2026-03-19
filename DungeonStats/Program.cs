cf1f943233b8a8372ef3ae9831dd6c4f9b10d67a
Joao Silva <joaosilvaa.work@gmail.com>
2026-03-19 16:08:52 +0000

    add branch core-logic

---
+++ DungeonStats/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DungeonStats
+{
+    public class Program
+    {
+
+        private static int Damage (int attack, int defense)
+        {
+            
+        }
+
+
+        private static int Damage (int attack)
+        {
+            
+        }
+
+        private static int CriticalHit(int damage)
+        {
+            
+        } 
+
+        
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
